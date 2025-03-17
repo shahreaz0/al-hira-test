14f5db92b3276429b8d8cf5d8937e37eb86b22f4
shahreaz0 <ashahreaz@gmail.com>
2025-03-18 00:33:56 +0600

    change openapi description

--- src/lib/configure-openapi.ts
+++ src/lib/configure-openapi.ts
@@ -8,9 +8,9 @@ export function configureOpenAPI(app: AppOpenAPI) {
     openapi: "3.0.0",
     info: {
       version: packageJSON.version,
-      title: "Actio API",
+      title: "Al-Hira API",
       description:
-        "This is a sample Pet Store Server based on the OpenAPI 3.0 specification.  You can find out more about\nSwagger at [https://swagger.io](https://swagger.io). \n\nSome useful links:\n- [Actio repository](https://github.com/shahreaz0/actio)",
+        "Al-Hira API",
     },
   })
 
